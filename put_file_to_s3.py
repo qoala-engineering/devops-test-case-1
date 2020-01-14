54ac153eba27679501967012d4d5e32216fb636a
sami <sami@qoala.id>
2020-01-14 09:31:12 +0700

    [WIP][ADD] Boto3 s3 client bucket test with localstack

--- put_file_to_s3.py
+++ put_file_to_s3.py
@@ -1,4 +1,32 @@
 import boto3
 from mypy_boto3 import s3
 
-s3_client = boto3.client("s3")
+try:
+    session = boto3.session.Session()
+
+    s3_client: s3.S3Client = session.client(
+        service_name="s3",
+        aws_access_key_id="123",
+        aws_secret_access_key="123",
+        endpoint_url="http://localhost:4572",
+    )
+
+    bucket_test1 = s3_client.create_bucket(Bucket="test1")
+    bucket_test2 = s3_client.create_bucket(Bucket="test2")
+
+    response = s3_client.list_buckets()
+    buckets = [bucket["Name"] for bucket in response["Buckets"]]
+    print(f"list buckets created: {buckets}")
+    if "test1" not in buckets or "test2" not in buckets:
+        raise Exception("Test bucket not created")
+
+    s3_client.delete_bucket(Bucket="test1")
+    s3_client.delete_bucket(Bucket="test2")
+
+    response = s3_client.list_buckets()
+    buckets = [bucket["Name"] for bucket in response["Buckets"]]
+    print(f"list buckets after delete: {buckets}")
+
+except Exception as e:
+    print(f"Error: {e}")
+
